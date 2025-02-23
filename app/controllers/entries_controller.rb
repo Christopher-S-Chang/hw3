8585555a1cd5c3ce0920806adf04cc4131247fbf
Christopher Chang <christopher.chang1@kellogg.northwestern.edu>
2025-02-23 01:36:49 +0000

    Done?

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,15 @@
 class EntriesController < ApplicationController
+def new
+end
+
+def create
+  entry = Entry.new 
+  entry["title"] = params["title"]
+  entry["occurred_on"] = params["occurred_on"]
+  entry["description"] = params["description"]
+  entry["place_id"] = params["place_id"]
+
+  redirect_to "/places/#{entry["place_id"]}"
+end
+
 end
